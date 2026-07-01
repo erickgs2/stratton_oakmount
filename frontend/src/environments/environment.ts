9fe4ec84108cf44081abd1c7da28b9d3ccf8822e
Erick Garcia <egarsev@gmail.com>
2026-07-01 14:16:49 -0600

    change the gateway redirection

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:3000/api',
-  gatewayLoginUrl: 'https://gsfawkes.duckdns.org:5002',
+  gatewayLoginUrl: 'https://gsfawkes.duckdns.org:5000',
 };
